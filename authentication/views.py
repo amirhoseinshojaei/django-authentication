b1a412e91655c41df8f8e2fe73e7a6c1666653f1
amirhosein <amirhoseinbu80@gmail.com>
2024-03-26 22:06:37 +0330

    create signup view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import SignupForm
 # Create your views here.
+
+def signup(request):
+    
+    if request.method=='POST':
+        signup_form = SignupForm(request.POST)
+        if signup_form.is_valid:
+            signup_form.save()
+            return redirect('login')
+    else:
+        signup_form = SignupForm()
+    context = {
+        'form':signup_form
+    }
+    return render (request,'authentication/signup.html',context)
