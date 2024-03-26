9d3ba0002bab7f44b7c2e4a84e50c553da04586e
amirhosein <amirhoseinbu80@gmail.com>
2024-03-26 22:05:32 +0330

    create signup form

---
+++ authentication/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=255)
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name','last_name','username', 'email', 'password1', 'password2'
+        )
