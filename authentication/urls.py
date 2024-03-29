dc57e32f59747ddcf5f2f337bdbc544f94d0d3b1
amirhosein <amirhoseinbu80@gmail.com>
2024-03-29 14:00:00 +0330

    create password change and change done

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from .import views
+from django.contrib.auth import views as auth_views
 
 
 app_name = 'authentication'
 
 urlpatterns = [
+    path("",views.home,name='home'),
     path("signup/",views.signup,name='signup'),
     path("login/",views.login,name='login'),
-    path("",views.home,name='home'),
+    path("password_change/",auth_views.PasswordChangeView.as_view(),name="password_change"),
+    path("password_change/done/",auth_views.PasswordChangeDoneView.as_view(),name="password_change_done"),
+
 ]
